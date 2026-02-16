6fcc4d43cef20d60066e7cd54ded4ebe78f088fd
Alexandr Zhdanko <lex.zhdanko@gmail.com>
2026-02-16 13:53:37 +0400

    feat(ui): add angular-svg-icon library and ICON constant
    - Installed and configured angular-svg-icon library
    - Created ICONS object with SVG paths
    - Added first icon (home.svg) for usage in components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AngularSvgIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
---
+++ src/app/shared/constants/icons.constant.ts
@@ -0,0 +1,4 @@
+const BASE_PATH = '/icons/';
+export const ICONS = {
+  home: `${BASE_PATH}home.svg`,
+} as const;
